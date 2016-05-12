7c37aa78374d0e4ee60dbbf8b648c816e37f7bbf
kzrs55 <569726429@qq.com>
2016-05-12 21:20:24 +0800

    Hello java

---
+++ src/ThinkInJava/First/Hello.java
@@ -0,0 +1,10 @@
+package ThinkInJava.First;
+
+/**
+ * Created by zjutK on 16/5/12.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
