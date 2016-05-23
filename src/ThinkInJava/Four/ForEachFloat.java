37afbf35c90d84d22f2e419fcac543351ce971e3
kzrs55 <569726429@qq.com>
2016-05-23 13:21:59 +0800

    foreach用法

---
+++ src/ThinkInJava/Four/ForEachFloat.java
@@ -0,0 +1,18 @@
+package ThinkInJava.Four;
+
+import java.util.Random;
+
+/**
+ * Created by zjutK on 16/5/23.
+ */
+public class ForEachFloat {
+    public static void main(String[] args) {
+        Random rand = new Random(47);
+        float f[] = new float[10];
+        for (int i = 0; i < 10; i++) {
+            f[i] = rand.nextFloat();
+        }
+        for (float x : f)
+            System.out.println(x);
+    }
+}
---
+++ src/ThinkInJava/Four/ForEachInt.java
@@ -0,0 +1,11 @@
+package ThinkInJava.Four;
+
+/**
+ * Created by zjutK on 16/5/23.
+ */
+public class ForEachInt {
+    public static void main(String[] args) {
+//        for(int i : range(10))
+//            System.out.print(i + " ");
+    }
+}
